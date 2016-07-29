6f0611bf5f4e4129dd74bb394f4cc6c169338f47
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2016-07-29 17:21:07 +0900

    쇼핑리스트 컴포넌트 세팅 완료

---
+++ src/components/AnimatedShoppingList.js
@@ -0,0 +1,54 @@
+/**
+ * Created by khk on 2016-07-29.
+ */
+import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+export default class AnimatedShoppingList extends Component {
+    constructor(){
+        super(...arguments);
+        this.state = {
+            items : [
+                { id : 1, name : 'Milk'},
+                { id : 2, name : 'Yogurt'},
+                { id : 3, name : 'Orange Juice'},
+            ]
+        };
+    }
+
+    handleChange(evt) {
+        if (evt.key === 'Enter') {
+            let newItem = { id : Date.now(), name : evt.target.value };
+            let newItems = this.state.items.concat(newItem);
+            evt.target.value = '';
+            this.setState({items : newItems});
+        }
+    }
+
+    handleRemove(i) {
+        var newItems = this.state.items;
+        newItems.splice(i,1);
+        this.setState({ items : newItems });
+    }
+
+    render() {
+        let shoppingItems = this.state.items.map( (item, i) => {
+            return (
+                <div key={item.id}
+                    className="item"
+                    onClick={this.handleRemove.bind(this, i)}>
+                    {item.name}
+                </div>
+            );
+        });
+        console.log(shoppingItems);
+        return (
+            <div>
+                {shoppingItems}
+                <input type="text" value={this.state.newItems}
+                    onKeyDown={this.handleChange.bind(this)}/>
+            </div>
+        );
+    }
+}
+
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import AnimatedShoppingList from './components/AnimatedShoppingList';
+import './index.css';
+
+render(<AnimatedShoppingList/>, document.getElementById("root"));
